7cb0b1778a61d49b13eb693528af41fcd2cdd620
lkss <lks.com>
2017-02-26 15:55:38 +0800

    主页

--- js/common/common.js
+++ js/common/common.js
@@ -5,4 +5,19 @@ define(['jquery'],function(){
     $('.navs .list-unstyled .nav a').on('click',function(){
         $(this).next().stop().slideToggle();
     })
+
+    $.ajax({
+        url: '/v6/login',
+        type: 'post',
+        data: {
+            tc_name: 123456,
+            tc_pass: 123456
+        },
+        success: function() {
+            console.log('≥…¡À')
+        },
+        error: function() {
+            console.log('∞‹¡À')
+        }
+    });
 })
