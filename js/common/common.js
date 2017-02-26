2d964e63652619f50ebaeee4571dc4c70bf14708
lkss <lks.com>
2017-02-26 10:14:24 +0800

    主页

--- js/common/common.js
+++ js/common/common.js
@@ -2,7 +2,7 @@
  * Created by likangshao on 2017/2/26.
  */
 define(['jquery'],function($){
-    $('.nav a').click(on,function(){
+    $('.navs .list-unstyled .nav a').click(on,function(){
         $(this).next().slideToggle();
     })
 })
