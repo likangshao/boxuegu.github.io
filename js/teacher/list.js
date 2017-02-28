962117d1b44d4b1f1ce726b57541bfb3cb25b5de
lkss <lks.com>
2017-03-01 00:31:50 +0800

    主页

--- js/teacher/list.js
+++ js/teacher/list.js
@@ -1,5 +1,27 @@
-define(['jquery', 'common', 'nprogress'], function($, undefined, nprogress) {
+define(['jquery', 'common', 'nprogress','template'], function($, undefined, nprogress,template) {
 
 	// 该页所有的js加载完毕，进度条结束。
 	nprogress.done();
+
+	//渲染讲师列表
+	$.get('/v6/teacher',function(data){
+		if(data.code==200){
+			var html=template('teacher-list-tpl',{list:data.result});
+			$('#teacher-list-tbody').html(html);
+		}
+	});
+
+	//通过事件委托的方式给动态生成的a标签绑定点击事件，
+	//然后获取讲师的详细信息并展示
+	$('#teacher-list-tbody').on('click','teacher-view',function(){
+		$.get('/v6/teacher/view',{
+			tc_id:$(this).parent().attr('data-id')
+		},function(data){
+			if(data.code==200){
+				var html=template('teacher-list-tpl',data.result);
+				$('#teacherModal').html(html);
+			}
+		})
+	})
+
 });
