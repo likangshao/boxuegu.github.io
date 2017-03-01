08c403707a70c5c213b8e416f8cd534644a107a1
lkss <lks.com>
2017-03-01 14:16:29 +0800

    主页6

--- js/teacher/list.js
+++ js/teacher/list.js
@@ -14,15 +14,32 @@ define(['jquery', 'common', 'nprogress','template'], function($, undefined, npro
 	//通过事件委托的方式给动态生成的a标签绑定点击事件，
 	//然后获取讲师的详细信息并展示
 	$('#teacher-list-tbody').on('click','.teacher-view',function(){
-		console.log('获取前');
 		$.get('/v6/teacher/view',{
 			tc_id:$(this).parent().attr('data-id')
 		},function(data){
-			console.log('获取后');
 			if(data.code==200){
 				var html=template('teacher-view-tpl',data.result);
 				$('#teacherModal').html(html);
-				console.log('展示');
+			}
+		})
+	})
+
+	//讲师状态修改:利用委托实现
+	$('#teacher-list-tbody').on('click','.teacher-status',function(){
+		var $self=$(this);
+		$.ajax({
+			url:'/v6/teacher/handle',
+			type:'post',
+			data:{
+				tc_id:$(this).parent().attr('data-id'),
+				tc_status:$(this).parent().attr('data-status')
+			},
+			success:function(data){
+				if(data.code==200){
+					// 得到修改后的状态，使用该状态修改按钮名称&父元素的data-status属性值
+					$self.html(data.result.tc_status == 0 ? '开启' : '注销');
+					$self.parent().attr('data-status',data.result.tc_status);
+				}
 			}
 		})
 	})
