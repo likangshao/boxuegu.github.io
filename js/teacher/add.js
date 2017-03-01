419ca0349e305544bc34d1cb34f94fa20c15f820
lkss <lks.com>
2017-03-01 18:38:04 +0800

    基本

--- js/teacher/add.js
+++ js/teacher/add.js
@@ -2,4 +2,19 @@ define(['jquery', 'common', 'nprogress'], function($, undefined, nprogress) {
 
 	// 该页所有的js加载完毕，进度条结束。
 	nprogress.done();
+
+	// 添加讲师
+	$('#teacher-add-form').on('submit',function(){
+		$.ajax({
+			url:'/v6/teacher/add',
+			type:'post',
+			data:$(this).serialize(),
+			success:function(data){
+				if(data.code==200){
+					location.href='/html/teacher/list.html';
+				}
+			}
+		})
+		return false;
+	});
 });
